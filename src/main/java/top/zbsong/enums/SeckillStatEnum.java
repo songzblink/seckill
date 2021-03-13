514b7562bbb899f15080842e3046053ad1916170
songzblink <songzblink@163.com>
2021-03-13 14:46:09 +0800

    利用枚举类来传递常量信息

---
+++ src/main/java/top/zbsong/enums/SeckillStatEnum.java
@@ -0,0 +1,44 @@
+package top.zbsong.enums;
+
+/**
+ * Create By songzb on 2021/3/13
+ */
+public enum SeckillStatEnum {
+    SUCCESS(1, "秒杀成功"),
+    END(0, "秒杀结束"),
+    REPEAT_KILL(-1, "重复秒杀"),
+    INNER_ERROR(-2, "系统异常"),
+    DATE_REWRITE(-3, "数据篡改");
+    private int state;
+    private String info;
+
+    SeckillStatEnum(int state, String info) {
+        this.state = state;
+        this.info = info;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public static SeckillStatEnum stateOf(int index) {
+        for (SeckillStatEnum state : values()) {
+            if (state.getState() == index) {
+                return state;
+            }
+        }
+        return null;
+    }
+}
